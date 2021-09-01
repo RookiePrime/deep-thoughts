93a358f29bd0fe449b56834c91c29e1b74421ffd
Kiefer Levine <kiefer_8@hotmail.com>
2021-09-01 14:16:54 -0400

    Added basic React front-end and concurrently module

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
+const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -18,6 +19,14 @@ app.use(express.json());
 
 server.start().then(res => {
   server.applyMiddleware({ app });
+
+  if (process.env.NODE_ENV === 'prduction') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 
   db.once('open', () => {
     app.listen(PORT, () => {
